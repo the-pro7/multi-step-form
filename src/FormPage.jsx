9975fd76f3b23c8b7c649dc33d7302f9656b54b4
Bean <alexanderafoko@gmail.com>
2023-08-25 10:15:38 +0000

    more changes

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -1,13 +1,15 @@
 import "./styleshseet/App.scss";
 import InputField from "./InputField";
 
-export default function App() {
+export default function FormPage() {
   let defaultProps = {
 
   }
 
   return (
     <>
+    <h1>Personal info</h1>
+    <h2></h2>
       <InputField {...{type: 'text', text: 'Name'}}/>
       <InputField {...{type: 'email', text: 'Email Address'}}/>
       <InputField {...{type: 'number', text: 'Phone Number'}}/>
