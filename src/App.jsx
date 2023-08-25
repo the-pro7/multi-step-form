288d75bc67b13e3daaee30aa3e68e5138c363b08
the-pro7 <ameyawemmanuel900@gmail.com>
2023-08-25 22:49:30 +0000

    Next step created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styleshseet/App.scss'
 import SideNav from './components/SideNav'
 import FormPage from "./components/FormPage"
+import PlanSelect from './components/PlanSelect'
 
 const App = () => {
 
@@ -8,7 +9,7 @@ const App = () => {
    <div className="form-container">
     <SideNav />
     <FormPage />
-    <FormPage />
+    <PlanSelect />
    </div>
   )
 }
