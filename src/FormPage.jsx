dca2e1066e81ca722f5a0488488241687c4f4a2c
Bean <alexanderafoko@gmail.com>
2023-08-29 09:54:11 +0000

    random changes

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -6,11 +6,6 @@ export default function FormPage() {
   return (
     <main className="form">
       <h1>Personal info</h1>
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur,
-        cumque! Numquam id eum facere labore.
-      </h2>
-      <h2></h2>
       <InputField {...{ type: "text", text: "Name" }} />
       <InputField {...{ type: "email", text: "Email Address" }} />
       <InputField {...{ type: "number", text: "Phone Number" }} />
